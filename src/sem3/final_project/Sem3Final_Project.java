8a67de63f3a70fe5c300a3ae168ba09ef7bdff70
Z.G. <zeel.gajjar2007@gmail.com>
2025-10-22 21:58:14 -0400

    Initial project files commit

---
+++ src/sem3/final_project/Sem3Final_Project.java
@@ -0,0 +1,27 @@
+/*
+ * Github: https://github.com/ZeelDGajjar/Sem3-Final_Project.git
+ */
+package sem3.final_project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author zeelg
+ */
+public class Sem3Final_Project extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    
+    }
+    
+}
