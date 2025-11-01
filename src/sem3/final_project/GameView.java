499085f8114fcefb999a650aef9614322e7e5c18
Vedika894 <vjain894@yahoo.com>
2025-10-31 23:16:40 -0400

    added the gameView and listed what it must implement as comment

---
+++ src/sem3/final_project/GameView.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sem3.final_project;
+
+/**
+ *
+ * @author Vedika
+ */
+
+//display all what the user sees during gamePlay 
+//no physics calculations 
+//user input , speed input TextField, angleSlider, slider, launch button 
+//something to draw projectile path 
+//level progression (label)
+//timer (label)
+//result feedback showZombied() animation, hit effects
+//styling 
+//score 
+
+
+public class GameView {
+    
+}
