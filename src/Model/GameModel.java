3d797d33ff140b403c7a163dfe98d9d17e379b47
Z.G. <zeel.gajjar2007@gmail.com>
2025-11-05 15:12:58 -0500

    Organizing the files

---
+++ src/Model/GameModel.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Model.GameState;
+
+/**
+ *
+ * @author Vedika
+ */
+class GameModel {
+       private GameState gameState;
+    
+    // Constructor that receives the GameState
+    public GameModel(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    // Example placeholder methods
+    public void startLevel() {
+        System.out.println("Level started.");
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+}
+
