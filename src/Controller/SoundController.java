e36ea3968b33e6a66be01587df651e77927445f7
Vedika894 <vjain894@yahoo.com>
2025-11-21 22:17:27 -0500

    added code for the sound

--- src/Controller/SoundController.java
+++ src/Controller/SoundController.java
@@ -4,10 +4,55 @@
  */
 package Controller;
 
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 /**
  *
- * @author zeelg
+ * @author Vedika 
  */
 class SoundController {
+    private MediaPlayer backgroundMusic; 
+    private MediaPlayer music; 
     
+    //the filePath of the music 
+    //i put it inside of view for now //is inside of view for now 
+    
+    //for now i add the one i added in my assigment but need to change 
+    /**
+     * Plays background music on loop.
+     */
+    public void playBackgroundMusic() {
+        try {
+            Media media = new Media(getClass().getResource("/View/slideshow.mp3").toExternalForm());
+            backgroundMusic = new MediaPlayer(media);
+            backgroundMusic.setCycleCount(MediaPlayer.INDEFINITE);
+            backgroundMusic.play();
+        } catch (Exception e) {
+            System.out.println("Error loading background music: " + e.getMessage());
+        }
+    }
+    
+    /**
+     * Stops background music.
+     */
+    public void stopBackgroundMusic() {
+        if (backgroundMusic != null) {
+            backgroundMusic.stop();
+        }
+    }
+
+    /**
+     * Plays a short sound effect (like level up or hit).
+     */
+    public void playSoundEffect(String filePath) {
+        try {
+            Media media = new Media(getClass().getResource(filePath).toExternalForm());
+            music = new MediaPlayer(media);
+            music.play();
+        } catch (Exception e) {
+            System.out.println("Error loading sound effect: " + e.getMessage());
+        }
+    }
 }
+
