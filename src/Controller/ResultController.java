772f0e2b8178f038416b9bfda718352f4d524340
Vedika894 <vjain894@yahoo.com>
2025-11-29 13:56:27 -0500

    change with the right import statement for label in resultController

--- src/Controller/ResultController.java
+++ src/Controller/ResultController.java
@@ -4,10 +4,12 @@
  */
 package Controller;
 
-import java.lang.classfile.Label;
+
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
