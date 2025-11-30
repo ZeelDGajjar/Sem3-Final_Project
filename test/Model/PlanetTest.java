287b26d4ddde057d322a44bd0ec117635f754b48
Vedika894 <vjain894@yahoo.com>
2025-11-30 13:31:29 -0500

    update planetTest according to new contrcutor

--- test/Model/PlanetTest.java
+++ test/Model/PlanetTest.java
@@ -10,40 +10,37 @@ class PlanetTest {
     private Planet planet;
     private Projectile projectile;
 
-    @BeforeEach
-    void setUp() {
-        // Planet at (0,0) radius 10, mass 100
-        planet = new Planet("Earth", 0, 0, 10, 100, true);
-        // Projectile at origin by default
-        projectile = new Projectile(0, 0, 1.0, new Vector2(0,0));
-    }
 
     @Test
     void testConstructorAndGetters() {
-        assertEquals("Earth", planet.getName());
+        Planet planet = new Planet("Earth",0,0,10,100);
         assertEquals(0, planet.getX());
         assertEquals(0, planet.getY());
         assertEquals(10, planet.getRadius());
         assertEquals(100, planet.getMass());
-        assertTrue(planet.isTarget());
+        assertTrue(planet.isTarget()); //default is false since it's not set in constructor
     }
 
     @Test
     void testCheckHit_Hit() {
-       //test hit at the edge of the planet  
-      projectile.setPosition(new Vector2(10, 0)); // exactly on the edge
-      assertTrue(planet.checkHit(projectile), "Projectile should hit at the edge of the radius");
+       Planet planet = new Planet("Earth", 0, 0, 10, 100);
+       Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(10, 0)); // exactly on edge
+
+        assertTrue(planet.checkHit(projectile), "Projectile should hit at the edge of the radius");
     }
 
     @Test
     void testCheckHit_Miss() {
-        // Projectile far away
-        projectile.setPosition(new Vector2(100, 100));
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+        Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(100, 100)); // far away
         assertFalse(planet.checkHit(projectile));
-    }
+       }
 
     @Test
     void testIncreaseRadiusFactor() {
+        
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         planet.increaseRadiusFactor(2);
         assertEquals(20, planet.getRadius(), "Radius should double");
 
@@ -54,20 +51,26 @@ class PlanetTest {
 
     @Test
     void testDecreaseRadiusFactor() {
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         planet.decreaseRadiusFactor(2);
         assertEquals(5, planet.getRadius());
     }
 
     @Test
     void testGenerateRandomPosition() {
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         planet.generateRandomPosition(2);
-        // Should be between 200 and 600 (min=100*2, max=300*2)
+        // Should be between 200 and 600 (min is 100*2, max is 300*2)
         assertTrue(planet.getX() >= 200 && planet.getX() <= 600);
         assertTrue(planet.getY() >= 200 && planet.getY() <= 600);
     }
 
     @Test
     void testCalculateGravityAt() {
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         Vector2 point = new Vector2(3, 4); // distance = 5
         double expectedGravity = 100 / (5 * 5); // mass / distance^2
         assertEquals(expectedGravity, planet.calculateGravityAt(point), 1e-10);
@@ -75,12 +78,16 @@ class PlanetTest {
 
     @Test
     void testCalculateGravityAtZeroDistance() {
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         Vector2 point = new Vector2(0,0); // same as planet position
         assertEquals(0, planet.calculateGravityAt(point));
     }
 
     @Test
     void testToString() {
+        Planet planet = new Planet("Earth", 0, 0, 10, 100);
+
         String str = planet.toString();
         assertTrue(str.contains("Earth"));
         assertTrue(str.contains("radius=10"));
