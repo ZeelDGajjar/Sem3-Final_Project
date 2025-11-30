b3e29cbcaa0309388b6d526cba4baab8bc13758d
Vedika894 <vjain894@yahoo.com>
2025-11-30 13:43:38 -0500

    change the test methods in collisonUtil

--- test/Model/CollisionUtilTest.java
+++ test/Model/CollisionUtilTest.java
@@ -13,30 +13,33 @@ class CollisionUtilTest {
     private Planet planet;
     private Projectile projectile;
 
-    @BeforeEach
-    void setUp() {
-        // Planet at (50,50) radius 10
-        planet = new Planet("Earth", 50, 50, 10, 100);
-        // Projectile initially at (50,50)
-        projectile = new Projectile(0, 0, 1.0, new Vector2(50, 50));
-    }
 
     @Test
     void testCheckCollision_Hit() {
+        Planet planet = new Planet("Earth", 50, 50, 50, 100);
         assertTrue(CollisionUtil.checkCollision(projectile, planet), "Projectile should hit planet");
     }
     
     @Test
     void testCheckCollision_Miss() {
-        projectile.setPosition(new Vector2(-20, -20));
+        Planet planet = new Planet("Earth", 50, 50, 10, 100);
+        Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(-20, -20));
+
         assertFalse(CollisionUtil.checkCollision(projectile, planet), "Projectile should miss planet diagonally");
     }
 
     @Test
     void testCheckAnyCollision_Hit() {
+        
+        Planet planet1 = new Planet("Mars", 0, 0, 5, 50);
+        Planet planet2 = new Planet("Earth", 50, 50, 10, 100);
+        Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(50, 50));
+
+        assertFalse(CollisionUtil.checkCollision(projectile, planet), "Projectile should miss planet diagonally");
+        
         List<Planet> planets = new ArrayList<>();
-        planets.add(new Planet("Mars", 0, 0, 5, 50));
-        planets.add(planet); // second planet will be hit
+        planets.add(planet1);
+        planets.add(planet2); // second planet will be hit
 
         Planet hit = CollisionUtil.checkAnyCollsion(projectile, planets);
         assertNotNull(hit, "Should detect a hit");
@@ -45,13 +48,17 @@ class CollisionUtilTest {
 
     @Test
     void testCheckAnyCollision_None() {
-        List<Planet> planets = new ArrayList<>();
-        planets.add(new Planet("Mars", 0, 0, 5, 50));
-        planets.add(new Planet("Venus", 20, 20, 5, 50));
-        planets.add(new Planet("Jupiter", 80, 80, 10, 200));
+        Planet planet1 = new Planet("Mars", 0, 0, 5, 50);
+        Planet planet2 = new Planet("Venus", 20, 20, 5, 50);
+        Planet planet3 = new Planet("Jupiter", 80, 80, 10, 200);
 
-        // Place projectile far from all planets
-        projectile.setPosition(new Vector2(200, 200));
+        Projectile projectile = new Projectile(0,0,1.0, new Vector2(200,200));
+        
+        List<Planet> planets = new ArrayList<>();
+        planets.add(planet1);
+        planets.add(planet2);
+        planets.add(planet3);
+        
 
         Planet hit = CollisionUtil.checkAnyCollsion(projectile, planets);
         assertNull(hit, "Projectile should not hit any planet");
@@ -59,13 +66,13 @@ class CollisionUtilTest {
 
     @Test
     void testIsOutOfBounds_Inside() {
-        projectile.setPosition(new Vector2(50, 50));
+       Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(50, 50));
         assertFalse(CollisionUtil.isOutOfBounds(projectile, 100, 100), "Projectile inside bounds");
     }
 
     @Test
     void testIsOutOfBounds_Outside() {
-        projectile.setPosition(new Vector2(-10, 50));
+        Projectile projectile = new Projectile(0, 0, 1.0, new Vector2(-10, 50));
         assertTrue(CollisionUtil.isOutOfBounds(projectile, 100, 100), "Projectile outside bounds left");
 
         projectile.setPosition(new Vector2(50, -5));
@@ -77,4 +84,5 @@ class CollisionUtilTest {
         projectile.setPosition(new Vector2(50, 150));
         assertTrue(CollisionUtil.isOutOfBounds(projectile, 100, 100), "Projectile outside bounds bottom");
     }
-}
+    }
+
